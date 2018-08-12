d4e8c254ec3aec1c5b15064009af76a4f90c9496
Andrew M <beeengine@gmail.com>
2018-08-12 19:04:01 +0300

    google search stuff

--- app.rb
+++ app.rb
@@ -13,6 +13,14 @@ get /\/(en|ua|ru|es)\/?/ do
   slim :home, layout: false, locals: { l: settings.send(params['captures'].first) }
 end
 
+get '/google843e00e5e643230e' do
+  send_file 'views/google.html'
+end
+
+get '/favicon.ico' do
+  send_file 'public/images/favicon-16x16.png'
+end
+
 get '/*' do
   slim :"pages/#{params[:splat].first}"
 end
