f2517a5aa368fc9f484c1de1b4ea47182052dff4
Andrew M <beeengine@gmail.com>
2018-08-12 19:12:42 +0300

    robots.txt added

--- app.rb
+++ app.rb
@@ -5,6 +5,10 @@ require "sinatra/content_for"
 config_file 'config.yml'
 config_file 'locales/*.yml'
 
+before do
+  cache_control :public, :must_revalidate, max_age: 31536000
+end
+
 get '/' do
   slim :home, layout: false, locals: { l: settings.ua }
 end
@@ -13,12 +17,16 @@ get /\/(en|ua|ru|es)\/?/ do
   slim :home, layout: false, locals: { l: settings.send(params['captures'].first) }
 end
 
-get '/google843e00e5e643230e' do
+get '/google843e00e5e643230e.html' do
   send_file 'views/google.html'
 end
 
 get '/favicon.ico' do
   send_file 'public/images/favicon-16x16.png'
+end
+
+get 'robots.txt' do
+  send_file 'public/robots.txt'
 end
 
 get '/*' do
