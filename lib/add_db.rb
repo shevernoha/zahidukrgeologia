e2c4c3cb86f5febb5045a7dffdc41f9b91362612
Andrew M <beeengine@gmail.com>
2018-12-08 22:32:50 +0200

    update script

--- lib/add_db.rb
+++ lib/add_db.rb
@@ -1,7 +1,8 @@
 require 'sequel'
 require 'pg'
 
-DB = Sequel.connect(adapter: :postgres, database: 'zug', host: 'localhost')
+db = URI.parse(ENV['DATABASE_URL'])
+DB = Sequel.connect(adapter: :postgres, host: db.host, user: db.user, database: db.database, password: db.password)
 
 DB.create_table :posts do
   primary_key :id
